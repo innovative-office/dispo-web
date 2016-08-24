3eecdc0d0f5a77f0fc708e30defbe40e1d6d9c3b
tzhbami7 <michael.balsiger@outlook.com>
2016-08-24 23:40:30 +0000

    first commit

---
+++ app/adapters/application.js
@@ -0,0 +1,30 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+const { underscore, pluralize } = Ember.String;
+
+export default DS.JSONAPIAdapter.extend({
+  // namespace: 'api',
+  // if your rails app is on a different port from your ember app
+  // this can be helpful for development.
+  // in production, the host for both rails and ember should be the same.
+  // host: ENV.host,
+
+  // allows the multiword paths in urls to be underscored
+  pathForType: function(type) {
+    let underscored = underscore(type);
+    return pluralize(underscored);
+  },
+
+  // allows queries to be sent along with a findRecord
+  // hopefully Ember / EmberData will soon have this built in
+  // ember-data issue tracked here:
+  // https://github.com/emberjs/data/issues/3596
+  urlForFindRecord(id, modelName, snapshot) {
+    let url = this._super(...arguments);
+    let query = Ember.get(snapshot, 'adapterOptions.query');
+    if(query) {
+      url += '?' + Ember.$.param(query);
+    }
+    return url;
+  }
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('purchase-orders');
 });
 
 export default Router;
