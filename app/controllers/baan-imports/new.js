891a63fc277ea0e021af90e2bc5dc4021dfeeb40
Michael Balsiger <michael.balsiger@fusion-robot.ch>
2016-09-12 13:56:30 +0200

    router work new import

--- app/controllers/baan-imports/new.js
+++ app/controllers/baan-imports/new.js
@@ -3,8 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     getFileDataUrl: function(fileName, data) {
-      console.log(fileName);
-      console.log(data);
+      this.set('model.baanUpload', data);
+    },
+    saveModel() {
+      let self = this;
+      this.get('model').save().then(function() {
+        var newRecord = self.get('store').createRecord('baan-import', { });
+        self.set('model', newRecord);
+      });
     }
   }
 });
